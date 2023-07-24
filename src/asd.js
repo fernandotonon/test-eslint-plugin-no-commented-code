08fa95229c2732ea881a59cba5a3186556e4adf4
Fernando <tonon.fernando@hotmail.com>
2023-07-23 22:24:13 -0400

    initial commit

---
+++ src/asd.js
@@ -0,0 +1,34 @@
+//Valid
+function a(){
+var name = "John";
+function sayHello() { console.log("Hello"); }
+if (x === 42) { /* do something */ }
+var x = 10; /* some comment */
+return result;
+var obj = { key: "value" };
+var arr = [1, 2, 3];
+console.log("Logging something");
+// This is a valid comment
+/*
+    the code should return something like this:
+    "Hello World"
+*/
+/* eslint-disable */ 
+// import { add } from './test.js';
+// const name = "John";
+/* const age = 30; */
+// if (condition) { /* do something */ }
+/* eslint-enable */
+}
+//Invalid
+// import { add } from './test.js';
+// const name = "John";
+/* const age = 30; */
+// if (condition) { /* do something */ }
+/* return value; */
+// class Rectangle { /* class implementation */ }
+/* const sum = (a, b) => a + b; */
+// { key: "value" };
+// [1, 2, 3];
+// console.log("Debugging");
+// expect(result).toBe(5);
